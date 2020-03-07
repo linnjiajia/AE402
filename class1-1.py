da6a6b1b992b20af878cc75e83f26da960428ee8
linnjiajia <linn2jiajia@gmail.com>
2020-03-07 15:30:54 +0800

    呱呱呱

---
+++ class1-1.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 13:46:10 2020
+
+@author: linn2
+"""
+
+import random
+
+number = random.randrange(1, 21)
+
+wtf = int(input("請輸入數字"))
+
+if number == wtf:
+    print("好棒棒!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
