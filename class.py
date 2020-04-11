08781ad1c3a107f072c18b13fcb8f03025a9dc50
linnjiajia <linn2jiajia@gmail.com>
2020-04-11 13:55:00 +0800

    home work

--- class.py
+++ class.py
@@ -4,12 +4,6 @@
 """
 # 匯入pygame模組
 import pygame
-import random
-def color():
-    r = random.randrange(256)
-    g = random.randrange(256)
-    b = random.randrange(256)
-    return (r,g,b)
 
 # 定義一些會用到的顏色
 # 常數使用大寫
@@ -32,10 +26,11 @@ done = False
 
 # 創造一個clock控制畫面更新速度
 clock = pygame.time.Clock()
+
+
+player = pygame.image.load("images.jpg")
 x = 0
 y = 0
-siz = 50
-colo = (0,0,0)
 
 
 # -------- 主要的程式迴圈 -----------
@@ -44,25 +39,24 @@ while not done:
     for event in pygame.event.get(): # 從事件list中抓取事件
         if event.type == pygame.QUIT: # 當使用者按下結束
             done = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            x,y = pos
-            siz = 0
-            colo = color()
+            
+        
             
             
-            
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        x -= 1
+    if keys[pygame.K_d]:
+        x += 1
             # 將done變數設為True-->while迴圈將會結束
     # --- 繪圖的程式碼
     #       先將畫面塗滿底色(將原有畫面清掉)
     #       繪圖的程式要寫在這行後面，不然會被這行清掉
     
     
-    screen.fill(WHITE)
-    if siz <= 50:
-        siz += 1
-        pygame.draw.circle(screen, colo, (x,y), siz)
     
+    screen.fill(BLACK)
+    screen.blit(player, [x,y])
         
     # --- 更新畫面
     pygame.display.flip()
--- template.py
+++ template.py
@@ -4,12 +4,6 @@
 """
 # 匯入pygame模組
 import pygame
-import random
-def color():
-    r = random.randrange(256)
-    g = random.randrange(256)
-    b = random.randrange(256)
-    return (r,g,b)
 
 # 定義一些會用到的顏色
 # 常數使用大寫
@@ -25,51 +19,42 @@ pygame.init()
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("好棒棒遊戲")
+pygame.display.set_caption("我的遊戲")
 
 # 設定一個開關供迴圈使用
 done = False
 
 # 創造一個clock控制畫面更新速度
 clock = pygame.time.Clock()
-x = 0
-y = 0
-siz = 50
-colo = (0,0,0)
-
 
 # -------- 主要的程式迴圈 -----------
 while not done:
     # --- 事件迴圈 event loop
     for event in pygame.event.get(): # 從事件list中抓取事件
         if event.type == pygame.QUIT: # 當使用者按下結束
-            done = True
+            done = True # 將done變數設為True-->while迴圈將會結束
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            x,y = pos
-            siz = 0
-            colo = color()
-            
-            
-            
-            # 將done變數設為True-->while迴圈將會結束
+            print(pos)
+
+    # --- 程式的運算與邏輯
+    
+
     # --- 繪圖的程式碼
     #       先將畫面塗滿底色(將原有畫面清掉)
     #       繪圖的程式要寫在這行後面，不然會被這行清掉
+    screen.fill(BLACK)
+    pygame.draw.circle(screen, WHITE, (350,250), 200)
+
     
-    
-    screen.fill(WHITE)
-    if siz <= 50:
-        siz += 1
-        pygame.draw.circle(screen, colo, (x,y), siz)
-    
-        
     # --- 更新畫面
     pygame.display.flip()
 
     # --- 每秒鐘60個frame
-    clock.tick(80)
+    clock.tick(60)
 
 # 關閉式窗並離開程式
 pygame.quit()
+exit()
+
 
