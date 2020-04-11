e4844f7f3e9f88bbc0ad9551179d5f7eaf938dde
linnjiajia <linn2jiajia@gmail.com>
2020-04-11 15:25:51 +0800

    class4

--- template (1).py
+++ template (1).py
@@ -4,6 +4,7 @@
 """
 # 匯入pygame模組
 import pygame
+import random
 
 # 定義一些會用到的顏色
 # 常數使用大寫
@@ -19,7 +20,7 @@ pygame.init()
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("好棒棒遊戲")
+pygame.display.set_caption("我的遊戲")
 
 # 設定一個開關供迴圈使用
 done = False
@@ -27,47 +28,52 @@ done = False
 # 創造一個clock控制畫面更新速度
 clock = pygame.time.Clock()
 
-
-player = pygame.image.load("images.jpg")
-sound = pygame.mixer.Sound("laser5.ogg")
-x = 0
-y = 0
-
-
+boxes = []
+for _ in range(10):
+    x = random.randrange(size[0])
+    y = random.randrange(size[1])
+    boxes.append([x,y,True])
 # -------- 主要的程式迴圈 -----------
 while not done:
     # --- 事件迴圈 event loop
     for event in pygame.event.get(): # 從事件list中抓取事件
         if event.type == pygame.QUIT: # 當使用者按下結束
-            done = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            sound.play()
+            done = True # 將done變數設為True-->while迴圈將會結束
 
-            
-        
-            
-            
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        x -= 1
-    if keys[pygame.K_d]:
-        x += 1
-            # 將done變數設為True-->while迴圈將會結束
+    # --- 程式的運算與邏輯
+    
+
     # --- 繪圖的程式碼
     #       先將畫面塗滿底色(將原有畫面清掉)
     #       繪圖的程式要寫在這行後面，不然會被這行清掉
-    
-    
-    
     screen.fill(BLACK)
-    screen.blit(player, [x,y])
+    x1,y1 = pygame.mouse.get_pos()
+    
+    for i in range(len(boxes)):
+        x = boxes[i][0]
+        y = boxes[i][1]
         
+        if (x <= x1+32 <= x+32 or x <= x1 <= x+32) and \
+        (y <= y1+32 <= y+32 or y <= y1 <= y+32):
+           boxes[i][2] = False
+    
+
+    pygame.draw.rect(screen, RED, [x1,y1,32,32])
+    for i in range(len(boxes)):
+        x,y,d = boxes[i]
+        if d:
+            pygame.draw.rect(screen, WHITE, [x,y,32,32])
+    
+
+    
     # --- 更新畫面
     pygame.display.flip()
 
     # --- 每秒鐘60個frame
-    clock.tick(80)
+    clock.tick(60)
 
 # 關閉式窗並離開程式
 pygame.quit()
+exit()
+
 
